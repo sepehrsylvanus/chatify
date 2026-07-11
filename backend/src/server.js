7d66b7910f603123e721539bde799bc5a8310694
sepehrsylvanus <mr.forte.world@gmail.com>
2026-07-11 20:46:23 +0330

    deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,13 +20,11 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/{*path}", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+app.get("*path", (_, res) => {
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
